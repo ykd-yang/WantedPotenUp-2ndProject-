f7e1f80405dfca0d126fd9221e8e1e8a03b0f3f6
허지웅 <joe0617160@naver.com>
2025-08-28 16:26:00 +0900

    Working Firebase RankingData

--- Source/HomeRunClashCopy/Public/Ranking/RankingDataManager.h
+++ Source/HomeRunClashCopy/Public/Ranking/RankingDataManager.h
@@ -4,10 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "RankingData.h"
+#include "Interfaces/IHttpRequest.h"
 
 /**
  * 
  */
+DECLARE_DELEGATE_OneParam(FOnRankingLoaded, const TArray<FRankingData>&);
+
 class HOMERUNCLASHCOPY_API RankingDataManager
 {
 public:
@@ -17,4 +20,13 @@ public:
 	static FRankingArray LoadLocal(const FString& FileName);
 	static void SaveLocal(const FRankingArray& Data, const FString& FileName);
 	static void AddNewDataLocal(FRankingArray& Data, const FString& Name, const int32 Score, const int32 ThrowBall, const FString& FileName);
+
+	
+	static void LoadOnline();
+	static void SaveOnline(const FRankingData& Data);
+	static FOnRankingLoaded OnRankingLoaded;
+	
+private:
+	static void OnLoadRankingResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
+	static void OnSaveRankingResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
 };
